987a409ddd8369a9a85f1443f9de4d62bdd8fd62
Noam Kleiner <nmkleiner@gmail.com>
2018-12-24 17:01:06 +0200

    Updates

---
+++ precache-manifest.e636657471fbad3c232b884b69547f58.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/mr-bitcoin/static/media/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "cf8961040ad02dea6e94",
+    "url": "/mr-bitcoin/static/css/main.12fe5ae9.chunk.css"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/mr-bitcoin/static/media/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "99347d92d84b66b1d774",
+    "url": "/mr-bitcoin/static/js/1.99347d92.chunk.js"
+  },
+  {
+    "revision": "725ba66c854ee4b5ddf4",
+    "url": "/mr-bitcoin/static/js/runtime~main.725ba66c.js"
+  },
+  {
+    "revision": "e65d96d04378217b8b5caff2fb1b62cb",
+    "url": "/mr-bitcoin/static/media/fa-regular-400.e65d96d0.woff2"
+  },
+  {
+    "revision": "fc5252e568503bfb163233ec63b8a2ce",
+    "url": "/mr-bitcoin/static/media/fa-regular-400.fc5252e5.woff"
+  },
+  {
+    "revision": "8e9f0ee6d016526776c27f91fea55cca",
+    "url": "/mr-bitcoin/static/media/fa-regular-400.8e9f0ee6.eot"
+  },
+  {
+    "revision": "deb8619e67992d3fa088b5dcbb8cff36",
+    "url": "/mr-bitcoin/static/media/fa-regular-400.deb8619e.ttf"
+  },
+  {
+    "revision": "b89c6c51aa2999159a6667f97a46a346",
+    "url": "/mr-bitcoin/static/media/fa-brands-400.b89c6c51.woff2"
+  },
+  {
+    "revision": "cf8961040ad02dea6e94",
+    "url": "/mr-bitcoin/static/js/main.cf896104.chunk.js"
+  },
+  {
+    "revision": "dd4ff1196905f0585aef2c80486869e9",
+    "url": "/mr-bitcoin/static/media/fa-brands-400.dd4ff119.woff"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/mr-bitcoin/static/media/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "50a544b5ad4d512e8db3cdc089823a00",
+    "url": "/mr-bitcoin/static/media/fa-brands-400.50a544b5.ttf"
+  },
+  {
+    "revision": "7d43b9ae0b8bb1d0dc425d78dc577b0a",
+    "url": "/mr-bitcoin/static/media/fa-brands-400.7d43b9ae.eot"
+  },
+  {
+    "revision": "734248d027cbaac7f03c0d931ec95593",
+    "url": "/mr-bitcoin/static/media/fa-regular-400.734248d0.svg"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/mr-bitcoin/static/media/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/mr-bitcoin/static/media/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
+    "url": "/mr-bitcoin/static/media/fa-brands-400.fef43c1f.svg"
+  },
+  {
+    "revision": "99347d92d84b66b1d774",
+    "url": "/mr-bitcoin/static/css/1.8a255f8c.chunk.css"
+  },
+  {
+    "revision": "e9a4b52b04314d95b25011c2e6dd234f",
+    "url": "/mr-bitcoin/index.html"
+  }
+];
